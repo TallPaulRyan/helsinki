39db7c74acc94026aa919d56f85f07d06eb30a5b
Paul Ryan <pmryan@Pauls-MacBook-Pro.local>
2021-11-27 13:24:44 -0800

    courseinfo completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,51 +3,65 @@ import React from 'react'
 const Header = (props) => {
   return (
     <div>
-      <h1>Header: {props.course}</h1>
+      <h1>Header: {props.course.name}</h1>
     </div>
   )
 }
 
 const Content = (props) => {
+  console.log(props)
   return(
     <div>
-      <Part name={props.part1} count={props.count1}/>
-      <Part name={props.part2} count={props.count2}/>
-      <Part name={props.part3} count={props.count3}/>
+      <Part info={props.course.parts[0]}/>
+      <Part info={props.course.parts[1]}/>
+      <Part info={props.course.parts[2]}/>
     </div>
   )
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <div>
-      <h2>Part: {props.name}</h2>
-      <p>Excercise Count: {props.count}</p>
+      <h2>Part: {props.info.name}</h2>
+      <p>Excercise Count: {props.info.count}</p>
     </div>
   )
 }
 
+// Still need to fix total
 const Total = (props) => {
   return (
     <div>
-      <p>Total: {props.total}</p>
+      <h2>Total: {props.course.parts[0].count + props.course.parts[1].count + props.course.parts[2].count}</h2>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = { 
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        count: 10
+      },
+      {
+        name: 'Using props to pass data',
+        count: 7
+      },
+      {
+        name: 'State of a component',
+        count: 14
+      }
+    ]
+  }
   
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} count1={exercises1} count2={exercises2} count3={exercises3}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
